25b9121d1e5b51727e6dc13f551883fa72c65ae4
shipon islam <shiponkhan566@gmail.com>
2025-08-03 15:47:17 +0600

    fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Router from "./routers/Router";
 export default function App() {
   return (
-    <div className="mx-2 max-w-[800px] sm:mx-auto ">
+    <div className="mx-4 max-w-[800px] sm:mx-auto ">
       <Header />
       <Router />
       <Footer />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <h1 className="text-sm bg-teal-500 absolute w-[96%] left-1/2 -translate-x-1/2 bottom-1.5 py-4 text-center text-teal-800 font-medium uppercase rounded-md">
+      <h1 className="text-sm bg-teal-500  py-4 text-center text-teal-800 font-medium uppercase rounded-md">
         Developed by Shipon islam
       </h1>
     </footer>
