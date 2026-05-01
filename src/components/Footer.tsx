74d8e278f9aea2b7accd4c2106117da95c0bd989
shipon islam <shiponkhan566@gmail.com>
2026-05-02 00:30:38 +0600

    fixed footer text issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <h1 className="text-sm bg-teal-500  py-4 text-center text-teal-800 font-medium uppercase rounded-md">
+      <h1 className="text-base bg-teal-500  py-4 text-center text-teal-800 font-medium uppercase rounded-md">
         Developed by Shipon islam
       </h1>
     </footer>
--- src/components/HomeSvg.tsx
+++ src/components/HomeSvg.tsx
@@ -4,7 +4,7 @@ const HomeSvg = ({ className }: { className?: string }) => {
     <div className={`my-4  ${className}`}>
       <img
         src={smartControl}
-        className="!w-full max-h-[340px] object-cover rounded-md"
+        className="!w-full max-h-[335px] object-cover rounded-md"
         alt="home-image"
       />
     </div>
